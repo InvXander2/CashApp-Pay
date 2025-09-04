ce8346ae2edcb2ddec7414a8117e008b09fde89a
InvXander2 <okpalaalexander12@gmail.com>
2025-09-04 07:35:34 +0000

    Update dbcon.php

--- config/dbcon.php
+++ config/dbcon.php
@@ -2,9 +2,9 @@
 //connection to mysql database
 
 $host = "sql100.infinityfree.com";  //database host
-$username = "if0_39543951";  //database user
-$password = "nHquHLJU41m6PHD";    //database password
-$database = "if0_39543951_cashapp";  //database name
+$username = "if0_39861603";  //database user
+$password = "26IRR3vVTzA";    //database password
+$database = "if0_39861603_cashapp_pay";  //database name
 
 $con = mysqli_connect("$host","$username","$password","$database");
 
